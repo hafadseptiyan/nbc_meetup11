de29b32b54013865d8985912f07c706317d2ca9c
hafadseptiyan <hafadseptiyan20@gmail.com>
2019-10-14 17:14:33 +0700

    initial commit

---
+++ database/migrations/2019_05_07_010427_create_author_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAuthorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');            
+            $table->string('photo');            
+            $table->text('bio');            
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('author');
+    }
+}
